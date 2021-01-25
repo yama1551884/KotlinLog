f1e04c3ea2fe8aff16d4c0b460ce67dcec9d9c11
yama1551884 <yama1551884@gmail.com>
2021-01-25 21:54:38 +0900

    dogを削除し、human2を追加。

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -10,19 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dog = Dog("ポチ", 3)
-        dog.say()
-        dog.move()
-        Log.d("Kotlintest", "犬の名前は" + dog.name + "です。")
-        Log.d("Kotlintest", "犬の年齢は" + dog.age + "歳です。")
-
-        val bigdog = BigDog("ヨーゼフ", 15)
-        bigdog.say()
-        Log.d("Kotlintest", "犬の名前は" + bigdog.name + "です。")
-        Log.d("Kotlintest", "犬の年齢は" + bigdog.age + "歳です。")
-
         val human = Human("山本",25,"サッカー")
         human.say()
         human.think()
+
+        val human2 = Human("裕貴",25,"野球")
+        human2.say()
+        human2.think()
     }
 }
